cda8fd1b5946e06825a4f90c0c848b248fddf819
joaquin vezzoni <joacovs@gmail.com>
2019-09-02 16:48:45 -0300

    1.2

--- nextUcodBaseJuego/js/app.js
+++ nextUcodBaseJuego/js/app.js
@@ -12,10 +12,39 @@ $(document).ready(function(){
 
 	CambiarColor();
 
-  
+  function CaidaDeDulces(){
+
+  for (var i = 1; i <7; i++) {
+    for (var j = 1; j < 7; j++) {
+      var tipoDulce= Math.floor((Math.random() * 4) + 1);
+      var fila = "<div class='row-"+j+"'></div>";
+      var elementoImg=document.createElement('img')
+      $(".col-"+i).append(elementoImg)
+      $(elementoImg).addClass('elemento')
+      $(elementoImg).attr('src',"image/"+tipoDulce+".png")
 
 
+     }
+  }
 
+
+}
+
+function agregarDulcesEvents() {
+  $('img').draggable({
+  containment: '.panel-tablero',
+  droppable: 'img',
+  revert: true,
+  revertDuration: 500,
+  grid: [100, 100],
+  zIndex: 10,
+  drag: constrainCandyMovement
+  });
+  $('img').droppable({
+    drop: moverDulce
+  });
+
+}
 
 
 
