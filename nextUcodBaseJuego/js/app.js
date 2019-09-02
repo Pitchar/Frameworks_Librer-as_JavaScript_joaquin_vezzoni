c5b0a1d7db54a30e5455a1fc308f5f819524780b
joaquin vezzoni <joacovs@gmail.com>
2019-09-02 16:21:28 -0300

    1.1

--- nextUcodBaseJuego/js/app.js
+++ nextUcodBaseJuego/js/app.js
@@ -1,2 +1,25 @@
-hhhhhhhhhhhhhh
-var 
+$(document).ready(function(){
+
+	function CambiarColor(elemento){
+		$(".main-titulo").css("color","white");
+		setTimeout(CambiarColor2,1000)
+	}
+
+	function CambiarColor2(elemento){
+		$(".main-titulo").css("color","yellow");
+		setTimeout(CambiarColor,1000)
+	}
+
+	CambiarColor();
+
+  
+
+
+
+
+
+
+
+
+
+});
