b65ce3fae67533dcfdfd91a82f13c4c633409e02
joaquin vezzoni <joacovs@gmail.com>
2019-09-02 17:10:09 -0300

    1.3

--- nextUcodBaseJuego/js/app.js
+++ nextUcodBaseJuego/js/app.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+var tiempoAtras;
+var min;
+var seg;
+var temporizador;
+var verifica = false;
+
 	function CambiarColor(elemento){
 		$(".main-titulo").css("color","white");
 		setTimeout(CambiarColor2,1000)
@@ -14,8 +20,8 @@ $(document).ready(function(){
 
   function CaidaDeDulces(){
 
-  for (var i = 1; i <7; i++) {
-    for (var j = 1; j < 7; j++) {
+  for (var i = 1; i <8; i++) {
+    for (var j = 1; j < 8; j++) {
       var tipoDulce= Math.floor((Math.random() * 4) + 1);
       var fila = "<div class='row-"+j+"'></div>";
       var elementoImg=document.createElement('img')
@@ -26,9 +32,27 @@ $(document).ready(function(){
 
      }
   }
-
-
+  agregarDulcesEvents();
+   HacerJugadaVertical();
+   HacerJugadaHorizontal();
 }
+
+function ReiniciarJuego(verifica) {
+  clearTimeout(temporizador);
+  $(".btn-reinicio").text("Iniciar");
+  if (verifica) {
+    $(".panel-tablero").show("slow");
+    $(".panel-score").animate(
+      {
+        width: "-=50"
+      }, 1000
+    );
+    verifica=false;
+  };
+}
+
+
+
 
 function agregarDulcesEvents() {
   $('img').draggable({
@@ -43,8 +67,45 @@ function agregarDulcesEvents() {
   $('img').droppable({
     drop: moverDulce
   });
-
+  HacerJugadaVertical();
+  HacerJugadaHorizontal();
 }
+
+$(".btn-reinicio").on("click", function(){
+  var nombre =$(".btn-reinicio").text();
+  if (nombre=="Iniciar") {
+    $(".btn-reinicio").text("Reiniciar");
+    clearTimeout(temporizador);
+  }
+  else {
+    ReiniciarJuego(verifica);
+  }
+  min =0;
+  seg =0;
+  $(".elemento").remove('img');
+  $('#score-text').text("0");
+  $('#movimientos-text').text("0");
+  $(".btn-reinicio").on("click", function(){
+    var nombre =$(".btn-reinicio").text();
+    if (nombre=="Iniciar") {
+      $(".btn-reinicio").text("Reiniciar");
+      clearTimeout(temporizador);
+    }
+    else {
+      ReiniciarJuego(verifica);
+    }
+    min =0;
+    seg =0;
+    $(".elemento").remove('img');
+    $('#score-text').text("0");
+    $('#movimientos-text').text("0");
+    updateReloj();
+    CaidaDeDulces();
+})
+})
+
+
+
 
 
 
